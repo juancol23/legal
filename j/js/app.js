5a788fc174e69d55b7633a91ef6dd5e0e5764e73
Juan Valdemar Colichon Ramirez <juanvcr@outlook.com>
2016-12-28 06:07:03 -0500

    Sumando la Web
    Jurisprudencia

---
+++ j/js/app.js
@@ -0,0 +1,108 @@
+var app = angular.module("app", []);
+
+app.factory("libros", function(){
+	return{
+		librosFactoria:[
+		{
+			id: 0,
+			titulo: "123",
+			especialidad:"Penal",
+			pretensionDelito: "Obligación de dar",
+			palabrasClaves: "Infracción normativa",
+			// img: "manuscritoAvicena.jpg"
+			url:"https://github.com/juanvaldemar/CV-Word/raw/master/Valdemarjuan280816%2B.docx",
+		 
+		},
+		{
+			id: 1,
+			titulo: "003108-2008",
+			especialidad:"Civil",
+			pretensionDelito: "Obligación de dar",
+			palabrasClaves: "sexo normativa",
+			// img: "manuscritoAvicena.jpg"
+			url:"https://github.com/juanvaldemar/CV-Word/raw/master/Valdemarjuan280816%2B.docx",
+		 
+		},
+		{
+			id: 2,
+			titulo: "003108-2008",
+			especialidad:"Civil",
+			pretensionDelito: "Obligación de dar",
+			palabrasClaves: "Infracción normativa",
+			// img: "manuscritoAvicena.jpg"
+			url:"https://github.com/juanvaldemar/CV-Word/raw/master/Valdemarjuan280816%2B.docx",
+		 
+		},
+		{
+			id: 3,
+			titulo: "003108-2008",
+			especialidad:"Penal",
+			pretensionDelito: "Obligación de sexo",
+			palabrasClaves: "sexo sexo",
+			// img: "manuscritoAvicena.jpg"
+			url:"https://github.com/juanvaldemar/CV-Word/raw/master/Valdemarjuan280816%2B.docx",
+		 
+		},
+		{
+			id: 4,
+			titulo: "003108-2008",
+			especialidad:"Civil",
+			pretensionDelito: "Obligación de dar",
+			palabrasClaves: "Infracción normativa",
+			// img: "manuscritoAvicena.jpg"
+			url:"https://github.com/juanvaldemar/CV-Word/raw/master/Valdemarjuan280816%2B.docx",
+		 
+		},
+		{
+			id: 5,
+			titulo: "003108-2008",
+			especialidad:"Penal",
+			pretensionDelito: "Obligación de dar",
+			palabrasClaves: "Infracción normativa",
+			// img: "manuscritoAvicena.jpg"
+			url:"https://github.com/juanvaldemar/CV-Word/raw/master/Valdemarjuan280816%2B.docx",
+		 
+		},
+		{
+			id: 6,
+			titulo: "003108-2008",
+			especialidad:"Civil",
+			pretensionDelito: "Obligación de dar",
+			palabrasClaves: "Infracción normativa",
+			// img: "manuscritoAvicena.jpg"
+			url:"https://github.com/juanvaldemar/CV-Word/raw/master/Valdemarjuan280816%2B.docx",
+		 
+		},
+		{
+			id: 7,
+			titulo: "003108-2008",
+			especialidad:"Civil",
+			pretensionDelito: "Obligación de dar",
+			palabrasClaves: "Infracción normativa",
+			// img: "manuscritoAvicena.jpg"
+			url:"https://github.com/juanvaldemar/CV-Word/raw/master/Valdemarjuan280816%2B.docx",
+		 
+		},
+		{
+			id: 8,
+			titulo: "003108-2008",
+			especialidad:"Penal",
+			pretensionDelito: "Obligación de dar",
+			palabrasClaves: "Infracción normativa",
+			// img: "manuscritoAvicena.jpg"
+			url:"https://github.com/juanvaldemar/CV-Word/raw/master/Valdemarjuan280816%2B.docx",
+		 
+		},
+		{
+			id: 9,
+			titulo: "003108-2008",
+			especialidad:"Civil",
+			pretensionDelito: "Obligación de dar",
+			palabrasClaves: "Infracción normativa",
+			// img: "manuscritoAvicena.jpg"
+			url:"https://github.com/juanvaldemar/CV-Word/raw/master/Valdemarjuan280816%2B.docx",
+		 
+		}
+		]
+	}
+})
