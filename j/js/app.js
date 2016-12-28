86e92d32ebc75bb8c00d42c220cb81839d1bce18
Juan Valdemar Colichon Ramirez <juanvcr@outlook.com>
2016-12-28 07:35:16 -0500

    assdasd
    asdfsdf

--- j/js/app.js
+++ j/js/app.js
@@ -98,7 +98,7 @@ app.factory("libros", function(){
 			titulo: "002759-2011-1400734572183",
 			especialidad:"Civil",
 			pretensionDelito: "ABANDONO DE MENOR",
-			palabrasClaves: "INCUMPLIMIENTO DE LOS REQUISITOS DE PROCEDENCIA",
+			palabrasClaves: "INCUMPLIMIENTO DE LOS REQUISITOS DE PROCEDESNCIA",
 			// img: "manuscritoAvicena.jpg"
 			url:"https://github.com/juanvaldemar/legal/raw/gh-pages/Jurisprudencia/Civil/ABANDONO%20DE%20MENOR/INCUMPLIMIENTO%20DE%20LOS%20REQUISITOS%20DE%20PROCEDENCIA/Resolucion%20002759-2011-1400734572183.pdf",
 		 
@@ -108,7 +108,7 @@ app.factory("libros", function(){
 			titulo: "002756-2010-1409467474259",
 			especialidad:"Civil",
 			pretensionDelito: "ABUSO DE DERECHO",
-			palabrasClaves: "INCUMPLIMIENTO DE LOS REQUISITOS DE PROCEDENCIA",
+			palabrasClaves: "INCUMPLIMIENTO DE LOS REQUISITOS DE PROCEDENSCIA",
 			// img: "manuscritoAvicena.jpg"
 			url:"https://github.com/juanvaldemar/legal/raw/gh-pages/Jurisprudencia/Civil/ABUSO%20DE%20DERECHO/INCUMPLIMIENTO%20DE%20LOS%20REQUISITOS%20DE%20PROCEDENCIA/Resolucion%20002756-2010-1409467474259.pdf",
 		 
