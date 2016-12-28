1bb722ffa7defa553b6c50a1eae7871a27336bd1
Juan Valdemar Colichon Ramirez <juanvcr@outlook.com>
2016-12-28 07:05:32 -0500

    jurisprudencia
    add

--- j/js/app.js
+++ j/js/app.js
@@ -4,7 +4,7 @@ app.factory("libros", function(){
 	return{
 		librosFactoria:[
 		{
-			id: 0,
+			
 			titulo: "Resolucion 000581-2012-1396330621387",
 			especialidad:"Civil",
 			pretensionDelito: "ABANDONO DE MENOR",
@@ -14,7 +14,7 @@ app.factory("libros", function(){
 		 
 		},
 		{
-			id: 1,
+		
 			titulo: "Resolucion 002759-2011-1400734572183",
 			especialidad:"Civil",
 			pretensionDelito: "ABANDONO DE MENOR",
@@ -24,169 +24,16 @@ app.factory("libros", function(){
 		 
 		},
 		{
-			id: 2,
-			titulo: "003108-2008",
+			
+			titulo: "Resolucion 002759-2011-1400734572183",
 			especialidad:"Civil",
-			pretensionDelito: "Obligación de dar",
-			palabrasClaves: "Infracción normativa",
-			// img: "manuscritoAvicena.jpg"
-			url:"https://github.com/juanvaldemar/legal/raw/gh-pages/Jurisprudencia/Civil/ABANDONO%20DE%20MENOR/INCUMPLIMIENTO%20DE%20LOS%20REQUISITOS%20DE%20PROCEDENCIA/Resolucion%20000581-2012-1396330621387.pdf",
-		 
-		},
-		{
-			id: 3,
-			titulo: "003108-2008",
-			especialidad:"Civil",
-			pretensionDelito: "Obligación de sexo",
-			palabrasClaves: "sexo sexo",
-			// img: "manuscritoAvicena.jpg"
-			url:"https://github.com/juanvaldemar/legal/raw/gh-pages/Jurisprudencia/Civil/ABANDONO%20DE%20MENOR/INCUMPLIMIENTO%20DE%20LOS%20REQUISITOS%20DE%20PROCEDENCIA/Resolucion%20000581-2012-1396330621387.pdf",
-		 
-		},
-		{
-			id: 4,
-			titulo: "003108-2008",
-			especialidad:"Civil",
-			pretensionDelito: "Obligación de dar",
-			palabrasClaves: "Infracción normativa",
-			// img: "manuscritoAvicena.jpg"
-			url:"https://github.com/juanvaldemar/legal/raw/gh-pages/Jurisprudencia/Civil/ABANDONO%20DE%20MENOR/INCUMPLIMIENTO%20DE%20LOS%20REQUISITOS%20DE%20PROCEDENCIA/Resolucion%20000581-2012-1396330621387.pdf",
-		 
-		},
-		{
-			id: 5,
-			titulo: "003108-2008",
-			especialidad:"Civil",
-			pretensionDelito: "Obligación de dar",
-			palabrasClaves: "Infracción normativa",
-			// img: "manuscritoAvicena.jpg"
-			url:"https://github.com/juanvaldemar/legal/raw/gh-pages/Jurisprudencia/Civil/ABANDONO%20DE%20MENOR/INCUMPLIMIENTO%20DE%20LOS%20REQUISITOS%20DE%20PROCEDENCIA/Resolucion%20000581-2012-1396330621387.pdf",
-		 
-		},
-		{
-			id: 6,
-			titulo: "003108-2008",
-			especialidad:"Civil",
-			pretensionDelito: "Obligación de dar",
-			palabrasClaves: "Infracción normativa",
-			// img: "manuscritoAvicena.jpg"
-			url:"https://github.com/juanvaldemar/legal/raw/gh-pages/Jurisprudencia/Civil/ABANDONO%20DE%20MENOR/INCUMPLIMIENTO%20DE%20LOS%20REQUISITOS%20DE%20PROCEDENCIA/Resolucion%20000581-2012-1396330621387.pdf",
-		 
-		},
-		{
-			id: 7,
-			titulo: "003108-2008",
-			especialidad:"Civil",
-			pretensionDelito: "Obligación de dar",
-			palabrasClaves: "Infracción normativa",
-			// img: "manuscritoAvicena.jpg"
-			url:"https://github.com/juanvaldemar/legal/raw/gh-pages/Jurisprudencia/Civil/ABANDONO%20DE%20MENOR/INCUMPLIMIENTO%20DE%20LOS%20REQUISITOS%20DE%20PROCEDENCIA/Resolucion%20000581-2012-1396330621387.pdf",
-		 
-		},
-		{
-			id: 8,
-			titulo: "003108-2008",
-			especialidad:"Civil",
-			pretensionDelito: "Obligación de dar",
-			palabrasClaves: "Infracción normativa",
-			// img: "manuscritoAvicena.jpg"
-			url:"https://github.com/juanvaldemar/legal/raw/gh-pages/Jurisprudencia/Civil/ABANDONO%20DE%20MENOR/INCUMPLIMIENTO%20DE%20LOS%20REQUISITOS%20DE%20PROCEDENCIA/Resolucion%20000581-2012-1396330621387.pdf",
-		 
-		},
-		{
-			id: 9,
-			titulo: "003108-2008",
-			especialidad:"Civil",
-			pretensionDelito: "Obligación de dar",
-			palabrasClaves: "Infracción normativa",
-			// img: "manuscritoAvicena.jpg"
-			url:"https://github.com/juanvaldemar/legal/raw/gh-pages/Jurisprudencia/Civil/ABANDONO%20DE%20MENOR/INCUMPLIMIENTO%20DE%20LOS%20REQUISITOS%20DE%20PROCEDENCIA/Resolucion%20000581-2012-1396330621387.pdf",
-		 
-		},
-		{
-			id: 10,
-			titulo: "003108-2008",
-			especialidad:"Administrativo Contencioso",
-			pretensionDelito: "Obligación de dar",
-			palabrasClaves: "Infracción normativa",
+			pretensionDelito: "ABANDONO DE MENOR",
+			palabrasClaves: "INCUMPLIMIENTO DE LOS REQUISITOS DE PROCEDENCIA",
 			// img: "manuscritoAvicena.jpg"
 			url:"",
 		 
-		},
-		{
-			id: 11,
-			titulo: "003108-2008",
-			especialidad:"Acuerdo Plenarios en Materia Laboral",
-			pretensionDelito: "Obligación de dar",
-			palabrasClaves: "Infracción normativa",
-			// img: "manuscritoAvicena.jpg"
-			url:""
-		},
-		{
-			id: 12,
-			titulo: "003108-2008",
-			especialidad:"Acuerdo Plenarios en Materia Legal",
-			pretensionDelito: "Obligación de dar",
-			palabrasClaves: "Infracción normativa",
-			// img: "manuscritoAvicena.jpg"
-			url:""
-		},
-		{
-			id: 13,
-			titulo: "003108-2008",
-			especialidad:"Concencioso Administrativo",
-			pretensionDelito: "Obligación de dar",
-			palabrasClaves: "Infracción normativa",
-			// img: "manuscritoAvicena.jpg"
-			url:""
-		},
-		{
-			id: 14,
-			titulo: "003108-2008",
-			especialidad:"Constitucional",
-			pretensionDelito: "Obligación de dar",
-			palabrasClaves: "Infracción normativa",
-			// img: "manuscritoAvicena.jpg"
-			url:""
 		}
-		,
-		{
-			id: 15,
-			titulo: "003108-2008",
-			especialidad:"Laboral",
-			pretensionDelito: "Obligación de dar",
-			palabrasClaves: "Infracción normativa",
-			// img: "manuscritoAvicena.jpg"
-			url:""
-		},
-		{
-			id: 16,
-			titulo: "003108-2008",
-			especialidad:"Plenos Casatorios",
-			pretensionDelito: "Obligación de dar",
-			palabrasClaves: "Infracción normativa",
-			// img: "manuscritoAvicena.jpg"
-			url:""
-		},
-		{
-			id: 17,
-			titulo: "003108-2008",
-			especialidad:"Precedentes Vinculantes Contencioso Administrativo",
-			pretensionDelito: "Obligación de dar",
-			palabrasClaves: "Infracción normativa",
-			// img: "manuscritoAvicena.jpg"
-			url:""
-		},
-		{
-			id: 18,
-			titulo: "003108-2008",
-			especialidad:"Previsional",
-			pretensionDelito: "Obligación de dar",
-			palabrasClaves: "Infracción normativa",
-			// img: "manuscritoAvicena.jpg"
-			url:""
-		}
-
+		 
 
 
 		]
