b37764ec307b7d5641aca00f2970c18f16693a82
BzAzuki <alex.misari@gmail.com>
2016-12-30 04:45:35 -0500

    0445
    0445

--- j/js/app.js
+++ j/js/app.js
@@ -44,14 +44,692 @@ app.factory("libros", function(){
 		 
 		},
 		{
-			
 			titulo: "002815-2011-1390773071915",
 			especialidad:"Civil",
 			pretensionDelito: "Abuso de Derecho",
 			palabrasClaves: "Incumplimiento de los requisitos de procedencia",
 			// img: "manuscritoAvicena.jpg"
 			url:"https://github.com/juanvaldemar/legal/raw/gh-pages/Jurisprudencia/Civil/ABUSO%20DE%20DERECHO/INCUMPLIMIENTO%20DE%20LOS%20REQUISITOS%20DE%20PROCEDENCIA/Resolucion%20002756-2010-1409467474259.pdf",
-		 
+		 },
+		{
+			titulo: "002815-2011-1390773071915",
+			especialidad:"Civil",
+			pretensionDelito: "Accesión de propiedad por edificación",
+			palabrasClaves: "Causal de nulidad",
+			// img: "manuscritoAvicena.jpg"
+			url:"",
+		 },
+		{
+			titulo: "002815-2011-1390773071915",
+			especialidad:"Civil",
+			pretensionDelito: "Accesión de propiedad por edificación",
+			palabrasClaves: "Inaplicación de la norma",
+			// img: "manuscritoAvicena.jpg"
+			url:"",
+		 },
+		{
+			titulo: "002815-2011-1390773071915",
+			especialidad:"Civil",
+			pretensionDelito: "Accesión de propiedad por edificación",
+			palabrasClaves: "Incumplimiento de los requisitos de procedencia",
+			// img: "manuscritoAvicena.jpg"
+			url:"",
+		 },
+		{
+			titulo: "002815-2011-1390773071915",
+			especialidad:"Civil",
+			pretensionDelito: "Accesión de propiedad por edificación",
+			palabrasClaves: "Incumplimiento de los requisitos de admisibilidad",
+			// img: "manuscritoAvicena.jpg"
+			url:"",
+		 },
+		{
+			titulo: "002815-2011-1390773071915",
+			especialidad:"Civil",
+			pretensionDelito: "Accesión de propiedad por edificación",
+			palabrasClaves: "Principio del debido proceso",
+			// img: "manuscritoAvicena.jpg"
+			url:"",
+		 },
+		{
+			titulo: "002815-2011-1390773071915",
+			especialidad:"Civil",
+			pretensionDelito: "Accesión de propiedad por edificación",
+			palabrasClaves: "Titulo de propiedad",
+			// img: "manuscritoAvicena.jpg"
+			url:"",
+		 },
+		{
+			titulo: "002815-2011-1390773071915",
+			especialidad:"Civil",
+			pretensionDelito: "Accesión de propiedad por edificación",
+			palabrasClaves: "Tutela jurisdiccional efectiva",
+			// img: "manuscritoAvicena.jpg"
+			url:"",
+		 },
+		{
+			titulo: "002815-2011-1390773071915",
+			especialidad:"Civil",
+			pretensionDelito: "Acción pauliana",
+			palabrasClaves: "Aplicación indebida de la norma",
+			// img: "manuscritoAvicena.jpg"
+			url:"",
+		 },
+		{
+			titulo: "002815-2011-1390773071915",
+			especialidad:"Civil",
+			pretensionDelito: "Acción pauliana",
+			palabrasClaves: "Derecho a la motivación",
+			// img: "manuscritoAvicena.jpg"
+			url:"",
+		 },
+		{
+			titulo: "002815-2011-1390773071915",
+			especialidad:"Civil",
+			pretensionDelito: "Acción pauliana",
+			palabrasClaves: "Incumplimiento de los requisitos de procedencia",
+			// img: "manuscritoAvicena.jpg"
+			url:"",
+		 },
+		{
+			titulo: "002815-2011-1390773071915",
+			especialidad:"Civil",
+			pretensionDelito: "Acción pauliana",
+			palabrasClaves: "Motivación de las resoluciones judiciales",
+			// img: "manuscritoAvicena.jpg"
+			url:"",
+		 },
+		{
+			titulo: "002815-2011-1390773071915",
+			especialidad:"Civil",
+			pretensionDelito: "Acción pauliana",
+			palabrasClaves: "Principio de congruencia procesal",
+			// img: "manuscritoAvicena.jpg"
+			url:"",
+		 },
+		{
+			titulo: "002815-2011-1390773071915",
+			especialidad:"Civil",
+			pretensionDelito: "AAcción pauliana",
+			palabrasClaves: "Requisitos de fondo",
+			// img: "manuscritoAvicena.jpg"
+			url:"",
+		 },
+		{
+			titulo: "002815-2011-1390773071915",
+			especialidad:"Civil",
+			pretensionDelito: "Acción pauliana",
+			palabrasClaves: "Tutela Jurisdiccional efectiva",
+			// img: "manuscritoAvicena.jpg"
+			url:"",
+		 },
+		{
+			titulo: "002815-2011-1390773071915",
+			especialidad:"Civil",
+			pretensionDelito: "Acción rescisoria",
+			palabrasClaves: "Contravención al debido proceso",
+			// img: "manuscritoAvicena.jpg"
+			url:"",
+		 },
+		{
+			titulo: "002815-2011-1390773071915",
+			especialidad:"Civil",
+			pretensionDelito: "Acción rescisoria",
+			palabrasClaves: "Incumplimiento de los requisitos de procedencia",
+			// img: "manuscritoAvicena.jpg"
+			url:"",
+		 },
+		{
+			titulo: "002815-2011-1390773071915",
+			especialidad:"Civil",
+			pretensionDelito: "Acción revocatoria",
+			palabrasClaves: "Incumplimiento de los requisitos de procedencia",
+			// img: "manuscritoAvicena.jpg"
+			url:"",
+		 },
+		{
+			titulo: "002815-2011-1390773071915",
+			especialidad:"Civil",
+			pretensionDelito: "Acción revocatoria",
+			palabrasClaves: "Debido proceso",
+			// img: "manuscritoAvicena.jpg"
+			url:"",
+		 },
+		{
+			titulo: "002815-2011-1390773071915",
+			especialidad:"Civil",
+			pretensionDelito: "Acción revocatoria",
+			palabrasClaves: "Mala fe",
+			// img: "manuscritoAvicena.jpg"
+			url:"",
+		 },
+		{
+			titulo: "002815-2011-1390773071915",
+			especialidad:"Civil",
+			pretensionDelito: "Acción revocatoria",
+			palabrasClaves: "Nulidad de sentencia",
+			// img: "manuscritoAvicena.jpg"
+			url:"",
+		 },
+		{
+			titulo: "002815-2011-1390773071915",
+			especialidad:"Civil",
+			pretensionDelito: "Acción revocatoria",
+			palabrasClaves: "Ineficacia del acto jurídico",
+			// img: "manuscritoAvicena.jpg"
+			url:"",
+		 },
+		{
+			titulo: "002815-2011-1390773071915",
+			especialidad:"Civil",
+			pretensionDelito: "Acción revocatoria",
+			palabrasClaves: "Garantía hipotecaria",
+			// img: "manuscritoAvicena.jpg"
+			url:"",
+		 },
+		{
+			titulo: "002815-2011-1390773071915",
+			especialidad:"Civil",
+			pretensionDelito: "Acción revocatoria",
+			palabrasClaves: "Inconducta procesal",
+			// img: "manuscritoAvicena.jpg"
+			url:"",
+		 },
+		{
+			titulo: "002815-2011-1390773071915",
+			especialidad:"Civil",
+			pretensionDelito: "Actualización de valor monetario",
+			palabrasClaves: "Bono agrario",
+			// img: "manuscritoAvicena.jpg"
+			url:"",
+		 },
+		{
+			titulo: "002815-2011-1390773071915",
+			especialidad:"Civil",
+			pretensionDelito: "Actualización de valor monetario",
+			palabrasClaves: "Debido proceso",
+			// img: "manuscritoAvicena.jpg"
+			url:"",
+		 },
+		{
+			titulo: "002815-2011-1390773071915",
+			especialidad:"Civil",
+			pretensionDelito: "Actualización de valor monetario",
+			palabrasClaves: "Incumplimiento de los requisitos de procedencia",
+			// img: "manuscritoAvicena.jpg"
+			url:"",
+		 },
+		{
+			titulo: "002815-2011-1390773071915",
+			especialidad:"Civil",
+			pretensionDelito: "Actualización de valor monetario",
+			palabrasClaves: "Rechazo de plano",
+			// img: "manuscritoAvicena.jpg"
+			url:"",
+		 },
+		{
+			titulo: "002815-2011-1390773071915",
+			especialidad:"Civil",
+			pretensionDelito: "Administración judicial de bienes",
+			palabrasClaves: "Debido proceso",
+			// img: "manuscritoAvicena.jpg"
+			url:"",
+		 },
+		{
+			titulo: "002815-2011-1390773071915",
+			especialidad:"Civil",
+			pretensionDelito: "Actualización de valor monetario",
+			palabrasClaves: "Inaplicación de la norma",
+			// img: "manuscritoAvicena.jpg"
+			url:"",
+		 },
+		{
+			titulo: "002815-2011-1390773071915",
+			especialidad:"Civil",
+			pretensionDelito: "Actualización de valor monetario",
+			palabrasClaves: "Incumplimiento de los requisitos de procedencia",
+			// img: "manuscritoAvicena.jpg"
+			url:"",
+		 },
+		{
+			titulo: "002815-2011-1390773071915",
+			especialidad:"Civil",
+			pretensionDelito: "Actualización de valor monetario",
+			palabrasClaves: "Masa hereditaria",
+			// img: "manuscritoAvicena.jpg"
+			url:"",
+		 },
+		{
+			titulo: "002815-2011-1390773071915",
+			especialidad:"Civil",
+			pretensionDelito: "Actualización de valor monetario",
+			palabrasClaves: "Valoración de la prueba",
+			// img: "manuscritoAvicena.jpg"
+			url:"",
+		 },
+		{
+			titulo: "002815-2011-1390773071915",
+			especialidad:"Civil",
+			pretensionDelito: "Actualización de valor monetario",
+			palabrasClaves: "Derecho de acción",
+			// img: "manuscritoAvicena.jpg"
+			url:"",
+		 },
+		{
+			titulo: "002815-2011-1390773071915",
+			especialidad:"Civil",
+			pretensionDelito: "Adopción",
+			palabrasClaves: "infracción normativa",
+			// img: "manuscritoAvicena.jpg"
+			url:"",
+		 },
+		{
+			titulo: "002815-2011-1390773071915",
+			especialidad:"Civil",
+			pretensionDelito: "Actualización de valor monetario",
+			palabrasClaves: "Interés superior del niño",
+			// img: "manuscritoAvicena.jpg"
+			url:"",
+		 },
+		{
+			titulo: "002815-2011-1390773071915",
+			especialidad:"Civil",
+			pretensionDelito: "Actualización de valor monetario",
+			palabrasClaves: "Bono agrario",
+			// img: "manuscritoAvicena.jpg"
+			url:"",
+		 },
+		{
+			titulo: "002815-2011-1390773071915",
+			especialidad:"Civil",
+			pretensionDelito: "Actualización de valor monetario",
+			palabrasClaves: "Requisitos de procedencia",
+			// img: "manuscritoAvicena.jpg"
+			url:"",
+		 },
+		{
+			titulo: "002815-2011-1390773071915",
+			especialidad:"Civil",
+			pretensionDelito: "ALimentos",
+			palabrasClaves: "Incumplimiento de los requisitos de procedencia",
+			// img: "manuscritoAvicena.jpg"
+			url:"",
+		 },
+		{
+			titulo: "002815-2011-1390773071915",
+			especialidad:"Civil",
+			pretensionDelito: "Anulabilidad del acto jurídico",
+			palabrasClaves: "infracción normativa",
+			// img: "manuscritoAvicena.jpg"
+			url:"",
+		 },
+		{
+			titulo: "002815-2011-1390773071915",
+			especialidad:"Civil",
+			pretensionDelito: "Anulabilidad del acto jurídico",
+			palabrasClaves: "Principio de congruencia",
+			// img: "manuscritoAvicena.jpg"
+			url:"",
+		 },
+		{
+			titulo: "002815-2011-1390773071915",
+			especialidad:"Civil",
+			pretensionDelito: "Anulabilidad del acto jurídico",
+			palabrasClaves: "Incumplimiento de los requisitos de procedencia",
+			// img: "manuscritoAvicena.jpg"
+			url:"",
+		 },
+		{
+			titulo: "002815-2011-1390773071915",
+			especialidad:"Civil",
+			pretensionDelito: "Anulabilidad del acto jurídico",
+			palabrasClaves: "Valoración de la prueba",
+			// img: "manuscritoAvicena.jpg"
+			url:"",
+		 },
+		{
+			titulo: "002815-2011-1390773071915",
+			especialidad:"Civil",
+			pretensionDelito: "Anulabilidad del acto jurídico",
+			palabrasClaves: "Principio del debido proceso",
+			// img: "manuscritoAvicena.jpg"
+			url:"",
+		 },
+		{
+			titulo: "002815-2011-1390773071915",
+			especialidad:"Civil",
+			pretensionDelito: "Anulabilidad del acto jurídico",
+			palabrasClaves: "Motivación de las resoluciones judiciales",
+			// img: "manuscritoAvicena.jpg"
+			url:"",
+		 },
+		{
+			titulo: "002815-2011-1390773071915",
+			especialidad:"Civil",
+			pretensionDelito: "Anulabilidad del acto jurídico",
+			palabrasClaves: "Plazo prescriptorio",
+			// img: "manuscritoAvicena.jpg"
+			url:"",
+		 },
+		{
+			titulo: "002815-2011-1390773071915",
+			especialidad:"Civil",
+			pretensionDelito: "Anulabilidad de matrimonio",
+			palabrasClaves: "Incumplimiento de los requisitos de procedencia",
+			// img: "manuscritoAvicena.jpg"
+			url:"",
+		 },
+		{
+			titulo: "002815-2011-1390773071915",
+			especialidad:"Civil",
+			pretensionDelito: "Anulabilidad laudo arbitral",
+			palabrasClaves: "Infracción normativa",
+			// img: "manuscritoAvicena.jpg"
+			url:"",
+		 },
+		{
+			titulo: "002815-2011-1390773071915",
+			especialidad:"Civil",
+			pretensionDelito: "Anulabilidad del acto jurídico",
+			palabrasClaves: "Incumplimiento de los requisitos de procedencia",
+			// img: "manuscritoAvicena.jpg"
+			url:"",
+		 },
+		{
+			titulo: "002815-2011-1390773071915",
+			especialidad:"Civil",
+			pretensionDelito: "Anulabilidad del acto jurídico",
+			palabrasClaves: "Incumplimiento de los requisitos de admisibilidad",
+			// img: "manuscritoAvicena.jpg"
+			url:"",
+		 },
+		{
+			titulo: "002815-2011-1390773071915",
+			especialidad:"Civil",
+			pretensionDelito: "Anulabilidad del acto jurídico",
+			palabrasClaves: "Valoración de los hechos",
+			// img: "manuscritoAvicena.jpg"
+			url:"",
+		 },
+		{
+			titulo: "002815-2011-1390773071915",
+			especialidad:"Civil",
+			pretensionDelito: "Anulabilidad del acto jurídico",
+			palabrasClaves: "Incumplimiento de los requisitos de admisibilidad",
+			// img: "manuscritoAvicena.jpg"
+			url:"",
+		 },
+		{
+			titulo: "002815-2011-1390773071915",
+			especialidad:"Civil",
+			pretensionDelito: "Anulación de testameto",
+			palabrasClaves: "Anulabilidad",
+			// img: "manuscritoAvicena.jpg"
+			url:"",
+		 },
+		{
+			titulo: "002815-2011-1390773071915",
+			especialidad:"Civil",
+			pretensionDelito: "Anulación de testameto",
+			palabrasClaves: "Incumplimiento de los requisitos de procedencia",
+			// img: "manuscritoAvicena.jpg"
+			url:"",
+		 },
+		{
+			titulo: "002815-2011-1390773071915",
+			especialidad:"Civil",
+			pretensionDelito: "Anulación de testameto",
+			palabrasClaves: "Inconducta procesal",
+			// img: "manuscritoAvicena.jpg"
+			url:"",
+		 },
+		{
+			titulo: "002815-2011-1390773071915",
+			especialidad:"Civil",
+			pretensionDelito: "Anulación de testameto",
+			palabrasClaves: "Valoración de la prueba",
+			// img: "manuscritoAvicena.jpg"
+			url:"",
+		 },
+		{
+			titulo: "002815-2011-1390773071915",
+			especialidad:"Civil",
+			pretensionDelito: "Anulación de testameto",
+			palabrasClaves: "Anulabilidad",
+			// img: "manuscritoAvicena.jpg"
+			url:"",
+		 },
+		{
+			titulo: "002815-2011-1390773071915",
+			especialidad:"Civil",
+			pretensionDelito: "Aprobación de cuentas",
+			palabrasClaves: "Incumplimiento de los requisitos de procedencia",
+			// img: "manuscritoAvicena.jpg"
+			url:"",
+		 },
+		{
+			titulo: "002815-2011-1390773071915",
+			especialidad:"Civil",
+			pretensionDelito: "Autenticidad de documento",
+			palabrasClaves: "Incumplimiento de los requisitos de procedencia",
+			// img: "manuscritoAvicena.jpg"
+			url:"",
+		 },
+		{
+			titulo: "002815-2011-1390773071915",
+			especialidad:"Civil",
+			pretensionDelito: "Autorización de viaje de menor",
+			palabrasClaves: "Incumplimiento de los requisitos de procedencia",
+			// img: "manuscritoAvicena.jpg"
+			url:"",
+		 },
+		{
+			titulo: "002815-2011-1390773071915",
+			especialidad:"Civil",
+			pretensionDelito: "Autorización de viaje de menor",
+			palabrasClaves: "Incumplimiento de los requisitos de procedencia",
+			// img: "manuscritoAvicena.jpg"
+			url:"",
+		 },
+		{
+			titulo: "002815-2011-1390773071915",
+			especialidad:"Civil",
+			pretensionDelito: "Autorización judicial",
+			palabrasClaves: "Incumplimiento de los requisitos de procedencia",
+			// img: "manuscritoAvicena.jpg"
+			url:"",
+		 },
+		{
+			titulo: "002815-2011-1390773071915",
+			especialidad:"Civil",
+			pretensionDelito: "Autorización judicial",
+			palabrasClaves: "Debido proceso",
+			// img: "manuscritoAvicena.jpg"
+			url:"",
+		 },
+		{
+			titulo: "002815-2011-1390773071915",
+			especialidad:"Civil",
+			pretensionDelito: "Autorización judicial",
+			palabrasClaves: "Infracción normativa",
+			// img: "manuscritoAvicena.jpg"
+			url:"",
+		 },
+		{
+			titulo: "002815-2011-1390773071915",
+			especialidad:"Civil",
+			pretensionDelito: "Autorización de demolición de obras",
+			palabrasClaves: "Incumplimiento de los requisitos de procedencia",
+			// img: "manuscritoAvicena.jpg"
+			url:"",
+		 },
+		{
+			titulo: "002815-2011-1390773071915",
+			especialidad:"Civil",
+			pretensionDelito: "Autorización de demolición de obras",
+			palabrasClaves: "Valoración probatoria",
+			// img: "manuscritoAvicena.jpg"
+			url:"",
+		 },
+		{
+			titulo: "002815-2011-1390773071915",
+			especialidad:"Civil",
+			pretensionDelito: "Autorización de demolición de obras",
+			palabrasClaves: "Incumplimiento de los requisitos de procedencia",
+			// img: "manuscritoAvicena.jpg"
+			url:"",
+		 },
+		{
+			titulo: "002815-2011-1390773071915",
+			especialidad:"Civil",
+			pretensionDelito: "Autorización de demolición de obras",
+			palabrasClaves: "Incumplimiento de los requisitos de procedencia",
+			// img: "manuscritoAvicena.jpg"
+			url:"",
+		 },
+		{
+			titulo: "002815-2011-1390773071915",
+			especialidad:"Civil",
+			pretensionDelito: "Auxilio judicial",
+			palabrasClaves: "Incumplimiento de los requisitos de procedencia",
+			// img: "manuscritoAvicena.jpg"
+			url:"",
+		 }
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	
+		
+		
+		
+	
+		
+		
+		
+	
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+			
+			
 
 
 		]
