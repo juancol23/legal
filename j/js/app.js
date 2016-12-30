bdc0a30336124e3642b858b2d2e8a114884daa31
BzAzuki <alex.misari@gmail.com>
2016-12-30 03:00:36 -0500

    0300
    0300

--- j/js/app.js
+++ j/js/app.js
@@ -7,8 +7,8 @@ app.factory("libros", function(){
 			
 			titulo: "000581-2012-1396330621387",
 			especialidad:"Civil",
-			pretensionDelito: "ABANDONO DE MENOR",
-			palabrasClaves: "INCUMPLIMIENTO DE LOS REQUISITOS DE PROCEDENCIA",
+			pretensionDelito: "Abandono de menor",
+			palabrasClaves: "Incumplimiento de los requisitos de procedencia",
 			// img: "manuscritoAvicena.jpg"
 			url:"https://github.com/juanvaldemar/legal/raw/gh-pages/Jurisprudencia/Civil/ABANDONO%20DE%20MENOR/INCUMPLIMIENTO%20DE%20LOS%20REQUISITOS%20DE%20PROCEDENCIA/Resolucion%20000581-2012-1396330621387.pdf",
 		 
@@ -17,8 +17,8 @@ app.factory("libros", function(){
 		
 			titulo: "002759-2011-1400734572183",
 			especialidad:"Civil",
-			pretensionDelito: "ABANDONO DE MENOR",
-			palabrasClaves: "INCUMPLIMIENTO DE LOS REQUISITOS DE PROCEDENCIA",
+			pretensionDelito: "Abandono de menor",
+			palabrasClaves: "Incumplimiento de los requisitos de procedencia",
 			// img: "manuscritoAvicena.jpg"
 			url:"https://github.com/juanvaldemar/legal/raw/gh-pages/Jurisprudencia/Civil/ABANDONO%20DE%20MENOR/INCUMPLIMIENTO%20DE%20LOS%20REQUISITOS%20DE%20PROCEDENCIA/Resolucion%20002759-2011-1400734572183.pdf",
 		 
@@ -27,22 +27,31 @@ app.factory("libros", function(){
 			
 			titulo: "002756-2010-1409467474259",
 			especialidad:"Civil",
-			pretensionDelito: "ABUSO DE DERECHO",
-			palabrasClaves: "INCUMPLIMIENTO DE LOS REQUISITOS DE PROCEDENCIA",
+			pretensionDelito: "Abandono de Derecho",
+			palabrasClaves: "Incumplimiento de los requisitos de procedencia",
 			// img: "manuscritoAvicena.jpg"
 			url:"https://github.com/juanvaldemar/legal/raw/gh-pages/Jurisprudencia/Civil/ABUSO%20DE%20DERECHO/INCUMPLIMIENTO%20DE%20LOS%20REQUISITOS%20DE%20PROCEDENCIA/Resolucion%20002756-2010-1409467474259.pdf",
 		 
 		},
 		{
 			
-			titulo: "002756-2010-1409467474259",
+			titulo: "002815-2011-1390773071915",
 			especialidad:"Civil",
-			pretensionDelito: "ABUSO DE DERECHO",
-			palabrasClaves: "INCUMPLIMIENTO DE LOS REQUISITOS DE PROCEDENCIA",
+			pretensionDelito: "Abuso de Derecho",
+			palabrasClaves: "Tasa judicial",
+			// img: "manuscritoAvicena.jpg"
+			url:"https://github.com/juanvaldemar/legal/raw/gh-pages/Jurisprudencia/Civil/ABUSO%20DE%20DERECHO/TASA%20JUDICIAL/Resolucion%20002815-2011-1390773071915.pdf",
+		 
+		},
+		{
+			
+			titulo: "002815-2011-1390773071915",
+			especialidad:"Civil",
+			pretensionDelito: "Abuso de Derecho",
+			palabrasClaves: "Incumplimiento de los requisitos de procedencia",
 			// img: "manuscritoAvicena.jpg"
 			url:"https://github.com/juanvaldemar/legal/raw/gh-pages/Jurisprudencia/Civil/ABUSO%20DE%20DERECHO/INCUMPLIMIENTO%20DE%20LOS%20REQUISITOS%20DE%20PROCEDENCIA/Resolucion%20002756-2010-1409467474259.pdf",
 		 
-		}
 
 
 		]
